3c76e191e847fa8a07723dd012b75d40c7303611
Lakshya Verma <lakshyav221@gmail.com>
2025-11-14 19:23:25 +0530

    port change

--- Backend/app.js
+++ Backend/app.js
@@ -94,10 +94,11 @@ mongoose
   .connect(urlDB)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(
-        `Server is running`
-      );
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
